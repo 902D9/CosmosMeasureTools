224ce1504c660f5b192f0e7fcf3260a57f775ec2
daizengtao <1711467513@qq.com>
2022-08-09 15:03:26 +0800

    创建插件

---
+++ Plugins/CosmosMeasureTools/Source/CosmosMeasureTools/Private/CosmosMeasureToolsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CosmosMeasureToolsBPLibrary.h"
+#include "CosmosMeasureTools.h"
+
+UCosmosMeasureToolsBPLibrary::UCosmosMeasureToolsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UCosmosMeasureToolsBPLibrary::CosmosMeasureToolsSampleFunction(float Param)
+{
+	return -1;
+}
+
